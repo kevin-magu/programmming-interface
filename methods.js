27849262841b5de61af513cae1f026462f5d76f7
kevin-magu <wanjaukevinmagu@gmail.com>
2025-05-15 22:00:20 +0300

    apis

--- methods.js
+++ methods.js
@@ -1,3 +1,25 @@
+//create a new post
+async function createPost(){
+const response  = await fetch ('https://jsonplaceholder.typicode.com/posts',{
+    METHOD: 'POST',
+    BODY:JSON.stringify({
+        title: 'Kevin New post',
+        body: 'Hello API wolrd',
+        userId: '1',
+    }),
+    headers:{
+        'Content-type': 'application/json',
+    },
+});
+if(response.ok){
+    console.log('This is a response ',await response.json());
+}else{
+    console.log('Creating a post failed');
+}
+
+}
+createPost()
+//get all posts
 async function getPosts() {
     const response = await fetch ('https://jsonplaceholder.typicode.com/posts');
     data = await response.json()
@@ -7,8 +29,24 @@ async function getPosts() {
 }
 getPosts()
 
-//create a new post
-async function createPost(){
-    const response  = await fetch ('https://jsonplaceholder.typicode.com/post');
+
+//get a single post
+async function getOnePost(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1',{
+        METHOD:'GET',
+        BODY: JSON.stringify({
+            userId: 1,
+        }),
+        headers:{
+            'Content-type': 'application/json',
+        },
+    });
     
+    if(response.ok){
+        const data =  await response.json();
+        console.log('this is one post',data)
+    }else{
+        console.log('There was an error in getting your post');
+    }
 }
+getOnePost()
