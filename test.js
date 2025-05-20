cdbdcef33ecc250fb6e88dbbf16908b5a739a43a
kevin-magu <wanjaukevinmagu@gmail.com>
2025-05-20 17:23:54 +0300

    js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log(Sum(Range(1, 10)));
+//factorial
+function factorial(n){
+    if(n==0){
+        return 1;
+    }else{
+        return factorial (n-1) * n;
+    }
+}
+console.log( 'Testing a factorial number',factorial(8));
