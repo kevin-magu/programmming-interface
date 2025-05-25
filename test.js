1a632deb6fb93015cc12eb6549a54b145ad4d6a5
kevin-magu <wanjaukevinmagu@gmail.com>
2025-05-25 15:39:44 +0300

    api

--- test.js
+++ test.js
@@ -9,4 +9,26 @@ function factorial(n){
 const fs = require('fs');
 
 //unary operators
-console.log(false || !false);
+const tellingLies = false;
+console.log(tellingLies? 'Jail': 'Give an Icecream');
+
+//empty values, null and undefined
+// they denote absence of a meaningful value,
+
+console.log(8 * null)
+console.log('nine' / 1)
+
+//binding or variable  , let
+let LuigisDebt = 140;
+let paid = 40;
+let LuigisDebtBalance = LuigisDebt - paid;
+
+console.log("Luigi's Balance is : ", LuigisDebtBalance);
+paid = 40;
+LuigisDebtBalance = LuigisDebtBalance - paid;
+console.log("Luigi's Balance is : ", LuigisDebtBalance);
+
+let man = 'Kevin', woman = 'Wanjiru', child = 'JOhn';
+console.log(man,woman,child)
+
+prompt('Hello kitty');
